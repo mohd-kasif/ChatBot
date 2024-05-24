7af0de5b163fab68416b5c32b4a594101886a73b
Mohd Kashif <kashifmd123@gmail.com>
2024-05-24 10:46:01 +0530

    Initial Commit

---
+++ ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by Mohd Kashif on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
